176943490f5afc5f2cb32bf7f8b433ef34bcc436
reginaseris-byte <regina.seris@gmail.com>
2026-03-31 18:24:50 +0000

    Deploying to gh-pages from @ reginaseris-byte/Regina-Seri-Atsebi@96ea0c571243ed2726fc64c91b471ae9e5f07e38 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "reginaseris-byte/Regina-Seri-Atsebi",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
